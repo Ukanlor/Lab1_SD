b240ea77bbf0dd03e12d6a6214659f8b47b2ae79
Ukanlor <134083059+Ukanlor@users.noreply.github.com>
2026-04-29 18:13:19 -0400

    Add files via upload

--- datos.py
+++ datos.py
@@ -2,35 +2,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-# Crear carpeta para guardar los gráficos si no existe
 os.makedirs("graficos", exist_ok=True)
 
 # =====================================================================
-# ¡ATENCIÓN! Cambia estas variables antes de cada nueva prueba
+# Cambiar estas variables antes de cada prueba
 # =====================================================================
 nombre_archivo_salida = "50MB_Random"
 titulo_grafico = "50MB - Random (Alternativa FIFO)"
 # =====================================================================
 
-# 1. Leer datos asumiendo que no hay encabezado para no perder la primera fila si son datos
 nombres_columnas = ["timestamp", "tipo_evento", "consulta", "latencia"]
 df = pd.read_csv('servicio_metricas/metricas.csv', header=None, names=nombres_columnas)
 
-# --- LIMPIEZA A PRUEBA DE BALAS ---
-# Si la primera fila es el texto del encabezado, la eliminamos
 df = df[df['timestamp'] != 'timestamp'].copy()
 
-# Convertir tipos de datos correctamente
 df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce')
 df['latencia'] = df['latencia'].astype(float)
-# ----------------------------------
 
-# 2. Calcular Throughput (Peticiones por segundo)
 tiempo_total_segundos = (df['timestamp'].max() - df['timestamp'].min()).total_seconds()
 total_reqs = len(df)
 throughput = total_reqs / tiempo_total_segundos if tiempo_total_segundos > 0 else 0
 
-# 3. Calcular Hit Rate y Latencias en milisegundos
 hits = len(df[df['tipo_evento'] == 'hit'])
 misses = len(df[df['tipo_evento'] == 'miss'])
 hit_rate = hits / total_reqs if total_reqs > 0 else 0
@@ -40,7 +32,7 @@ p95 = df['latencia'].quantile(0.95) * 1000
 df['latencia_ms'] = df['latencia'] * 1000
 
 # ---------------------------------------------------------
-# GRÁFICO 1: Hits vs Misses (Torta)
+# GRÁFICO 1: Hits vs Misses
 # ---------------------------------------------------------
 plt.figure(figsize=(6, 6))
 plt.pie([hits, misses], labels=['Hits', 'Misses'], autopct='%1.1f%%', colors=['#4CAF50', '#F44336'], startangle=90)
@@ -78,7 +70,7 @@ plt.savefig(f"graficos/latencia_consultas_{nombre_archivo_salida}.png", dpi=300,
 plt.close()
 
 # ---------------------------------------------------------
-# SALIDA PARA EL INFORME (Tabla LaTeX)
+# Tabla LaTeX
 # ---------------------------------------------------------
 print("\n" + "="*60)
 print(f"RESULTADOS GENERADOS PARA: {titulo_grafico}")
